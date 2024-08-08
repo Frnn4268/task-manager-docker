8c11c0e8a360343ecf0f98a5e879adbd523365f6
Fernando Martínez <fmartinezm7@miumg.edu.gt>
2024-08-07 22:58:37 -0600

    Adding new content in App.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TaskList from './pages/TaskList';
+import CreateTask from './pages/CreateTask';
+import EditTask from './pages/EditTask';
+import 'antd/dist/reset.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/create" element={<CreateTask />} />
+        <Route path="/edit/:id" element={<EditTask />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
